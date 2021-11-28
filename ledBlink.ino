283e41cd8ebbeda857e0e0fad6975b8dc3398c1f
nickboyko <nab9488@nyu.edu>
2021-11-28 16:11:20 -0500

    added toggle switch

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,16 +1,20 @@
 int ledPin = 13;
 int ledPin2 = 14;
+int switchPin = 15;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(switchPin, INPUT);
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH); 
-  digitalWrite(ledPin2, HIGH);
-  delay(500);
-  digitalWrite(ledPin, LOW); 
-  digitalWrite(ledPin2, LOW);
-  delay(500);
+  if (digitalRead(switchPin) == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, LOW);
+    delay(500);
+  }
 }
